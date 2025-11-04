2e61865bf4c046abc8ce3e99f20ff5b37b946d18
Ifeanyichukwu <nworieifeanyichukwustephen@gmail.com>
2025-11-04 06:40:52 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const mobile_menu = document.querySelector('.mobile-menu');
+const menuOpen = document.querySelector('.menu-open');
+const menuClose = document.querySelector('.menu-close');
+const overlay = document.querySelector('.overlay');
+
+// function to open menu 
+const openMenu = () => {
+    mobile_menu.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+// function to close menu 
+const closeMenu = () => {
+    mobile_menu.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+// apply open and close menu functions
+menuOpen.addEventListener('click', openMenu);
+menuClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
